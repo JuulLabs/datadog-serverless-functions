c97ea3f4c7707d22f9e30a68d81da40e38fb1509
Jesse Collier <jessecollier@gmail.com>
2021-02-10 16:15:32 -0500

    Fix package function

--- aws/logs_monitoring/tools/package-zip.sh
+++ aws/logs_monitoring/tools/package-zip.sh
@@ -11,14 +11,14 @@ if [ -z "$1" ]; then
     echo "Must specify a desired version number"
     exit 1
 else
-    if [[ $1 =~ ^[0-9]+\.[0-9]+\.[0-9]+ ]]; then
+    if [[ $1 =~ ^aws-dd-forwarder-[0-9]+\.[0-9]+\.[0-9]+ ]]; then
         VERSION=$1
-    elif [[ $1 =~ refs/tags/[0-9]+\.[0-9]+\.[0-9]+ ]]; then
+    elif [[ $1 =~ refs/tags/aws-dd-forwarder-[0-9]+\.[0-9]+\.[0-9]+ ]]; then
         VERSION="$(echo $1 | cut -d/ -f3)"
     else
-        echo "Must use a semantic version, e.g., 3.1.4"
+        echo "Must use a semantic version, e.g., aws-dd-forwarder-3.1.4"
         exit 1
     fi
 fi
 
-zip -r aws-dd-forwarder-$VERSION.zip ../lambda_function.py
+zip -r $VERSION.zip ../lambda_function.py
